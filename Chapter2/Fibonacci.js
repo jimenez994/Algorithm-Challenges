af0d92774139d842b4aea2abe73893b026e2f346
Jonathan Jimenez <j.jimenez.994@hotmail.com>
2018-06-25 10:04:52 -0400

    _

---
+++ Chapter2/Fibonacci.js
@@ -0,0 +1,24 @@
+// Fibonacci
+function fib(num) {
+  var fibs = [0,1]
+  if(num < 2){
+    console.log("fib of " + num + " = " + fibs[num])    
+    return fibs[num];
+  }
+  for (let i = 1; i < num; i++) {
+    fibs.push(fibs[fibs.length - 1] + fibs[fibs.length - 2] );
+  }
+  console.log("fib of " + num + " = " + fibs[fibs.length-1])
+  return fibs[fibs.length - 1];
+}
+// fib(1)
+
+// Fibonacci Recurse
+function fibR(num) {
+    if(num < 2){
+      console.log("Your fib + " + num)
+      return num;
+    }
+    return fibR(num-1) + fibR(num-2)
+}
+console.log(fibR(5))
