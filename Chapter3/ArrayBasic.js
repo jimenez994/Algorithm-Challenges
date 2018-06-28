09de5d007b55c493bd1f786b36f938c3482db45c
Jonathan Jimenez <j.jimenez.994@hotmail.com>
2018-06-28 18:10:24 -0400

    _

--- Chapter3/ArrayBasic.js
+++ Chapter3/ArrayBasic.js
@@ -1,9 +1,9 @@
-var arr = [23,45,2,4,5,5,5,66,7,7,8,98,2,34,0,34];
+var arr = [23, 45, 2, 4, 5, 5, 5, 66, 7, 7, 8, 98, 2, 34, 0, 34];
 
 // Push Front
-function pushFront(val,arr) {
+function pushFront(val, arr) {
   for (let i = arr.length; i > 0; i--) {
-    arr[i] = arr[i-1]    
+    arr[i] = arr[i - 1]
   }
   arr[0] = val
   console.log(arr)
@@ -14,15 +14,14 @@ function pushFront(val,arr) {
 function popFront(arr) {
   var result = arr[0];
   for (let i = 0; i < arr.length - 1; i++) {
-    arr[i] = arr[i+1];
+    arr[i] = arr[i + 1];
   }
-  arr.length = arr.length -1;
+  arr.length = arr.length - 1;
   console.log(result);
   console.log(arr);
   return result;
 }
 // popFront(arr);
-
 
 function insertAt(arr, indx, val) {
   arr.splice(indx, 0, val)
