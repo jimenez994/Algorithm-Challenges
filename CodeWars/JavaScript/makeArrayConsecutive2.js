c9eaad8dd197cb01d5f2e1a1993379759ad84473
Jonathan Jimenez <j.jimenez.994@hotmail.com>
2018-09-10 17:56:03 -0400

    _

---
+++ CodeWars/JavaScript/makeArrayConsecutive2.js
@@ -0,0 +1,7 @@
+function makeArrayConsecutive2(n) {
+  var dic = {0:"" ,1:"", 2:"", 3:"", 4:"", 5:"", 6:"", 7:"", 8:"", 9:""}
+  for(var i = 0; i<n; i++){
+    console.log(dic[i])
+  }
+}
+makeArrayConsecutive2(4)
